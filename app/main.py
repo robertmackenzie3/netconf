70fd3e3422316d9627c5c9bbd1892b3e69b4beb7
Robert Charles Mackenzie <robert.mackenzie3@sky.uk>
2024-05-22 09:11:50 +0100

    move connection logic to backend

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
 import logging
 import os
-from app.backend import create_device
 
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+
+from app.backend import ManagerParams, Interface
+from app.models import InterfaceConfig
+from app.exceptions import CannotEdit
+
 
 load_dotenv()
 
-log_level = getattr(
-    logging, os.getenv("LOG_LEVEL", "INFO").upper(), logging.DEBUG
-)
+log_level = getattr(logging, os.getenv("LOG_LEVEL", "INFO").upper(), logging.DEBUG)
 logging.basicConfig(level=log_level)
 logger = logging.getLogger(__name__)
 
@@ -18,66 +19,20 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 
 
-class InterfaceConfig(BaseModel):
-    interface_name: str
-    address: str
-    netmask: str
-    active: str = "act"
-
-
-# @app.put("/interface")
-# def create_interface(
-#     host: str, device_type: str, interface_config: InterfaceConfig
-# ):
-#     manager_params = ManagerParams(host=host)
-#     manager_params.set_device_params(device_type)
-#     template = env.get_template("iosxr_create_interface.xml.j2")
-#     rendered_config = template.render(**interface_config.__dict__)
-#     try:
-#         with manager.connect(**manager_params.__dict__) as conn:
-#             conn.edit_config(
-#                 target="candidate",
-#                 config=rendered_config
-#             )
-#             conn.commit()
-#             return {
-#                 "detail": (
-#                     f"Created {interface_config.interface_name} on {host}"
-#                 )
-#             }
-#     except Exception as e:
-#         logger.exception(e.__class__.__name__)
-#         raise HTTPException(
-#             status_code=500, detail=f"Exception {e.__class__.__name__}: {e}"
-#         )
-
-
-# @app.delete("/interface")
-# def delete_interface(host: str, device_type: str, interface_name: str):
-#     manager_params = ManagerParams(host=host)
-#     manager_params.set_device_params(device_type)
-#     template = env.get_template("iosxr_delete_interface.xml.j2")
-#     rendered_config = template.render(interface_name=interface_name)
-#     try:
-#         with manager.connect(**manager_params.__dict__) as conn:
-#             conn.edit_config(
-#                 target="candidate", config=rendered_config
-#             )
-#             conn.validate()
-#             conn.commit()
-#             return {"detail": f"Deleted {interface_name} on {host}"}
-#     except Exception as e:
-#         logger.exception(e.__class__.__name__)
-#         raise HTTPException(
-#             status_code=500, detail=f"Exception {e.__class__.__name__}: {e}"
-#         )
+@app.get("/healthz")
+def healthz():
+    """
+    Basic endpoint for use with health checks e.g. in k8s
+    """
+    return {"detail": "Netconf is running"}
 
 
 @app.get("/interface")
 def get_interface(host: str, device_type: str, interface_name: str):
-    device = create_device(host, device_type)
+    manager_params = ManagerParams(host, device_type)
+    interface = Interface(manager_params)
     try:
-        json_data = device.get_interface(interface_name)
+        json_data = interface.get_one(interface_name)
     except Exception as e:
         logger.exception(e.__class__.__name__)
         raise HTTPException(
@@ -92,23 +47,46 @@ def get_interface(host: str, device_type: str, interface_name: str):
         )
 
 
-# @app.get("/interfaces")
-# def get_interfaces(host: str, device_type: str):
-#     manager_params = ManagerParams(host=host)
-#     manager_params.set_device_params(device_type)
-#     try:
-#         with manager.connect(**manager_params.__dict__) as conn:
-#             filter = """
-#             <filter>
-#                 <interface-configurations xmlns="http://cisco.com/ns/yang/Cisco-IOS-XR-ifmgr-cfg"/>
-#             </filter>
-#             """
-#             response = conn.get_config(source="running", filter=filter)
-#             xml_data = response.data_xml
-#             json_data = xmltodict.parse(xml_data)
-#             return json_data
-#     except Exception as e:
-#         logger.exception(e.__class__.__name__)
-#         raise HTTPException(
-#             status_code=500, detail=f"Exception {e.__class__.__name__}: {e}"
-#         )
+@app.get("/interfaces")
+def get_interfaces(host: str, device_type: str):
+    manager_params = ManagerParams(host, device_type)
+    interface = Interface(manager_params)
+    try:
+        return interface.get_all()
+    except Exception as e:
+        logger.exception(e.__class__.__name__)
+        raise HTTPException(
+            status_code=500, detail=f"Exception {e.__class__.__name__}: {e}"
+        )
+
+
+@app.put("/interface")
+def add_interface(host: str, device_type: str, interface_config: InterfaceConfig):
+    manager_params = ManagerParams(host, device_type)
+    interface = Interface(manager_params)
+    try:
+        return interface.add(interface_config)
+    except CannotEdit as e:
+        logger.info(str(e))
+        raise HTTPException(status_code=400, detail=f"Cannot edit: {e}")
+    except Exception as e:
+        logger.exception(e.__class__.__name__)
+        raise HTTPException(
+            status_code=500, detail=f"Exception {e.__class__.__name__}: {e}"
+        )
+
+
+@app.delete("/interface")
+def remove_interface(host: str, device_type: str, interface_name: str):
+    manager_params = ManagerParams(host, device_type)
+    interface = Interface(manager_params)
+    try:
+        return interface.remove(interface_name)
+    except CannotEdit as e:
+        logger.info(str(e))
+        raise HTTPException(status_code=400, detail=f"Cannot edit: {e}")
+    except Exception as e:
+        logger.exception(e.__class__.__name__)
+        raise HTTPException(
+            status_code=500, detail=f"Exception {e.__class__.__name__}: {e}"
+        )
