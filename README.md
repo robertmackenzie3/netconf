2a3a5f0ac1bb42e8dd9beadc2a824f62f50070f8
Robert Charles Mackenzie <robert.mackenzie3@sky.uk>
2024-05-24 19:14:20 +0100

    add integration tests

--- README.md
+++ README.md
@@ -24,6 +24,38 @@ Uses netconf to make changes on network devices
 Serving at: <http://127.0.0.1:8000>  
 API docs: <http://127.0.0.1:8000/docs>
 
+## Unit tests
+
+1. Run `./run_tests.sh`
+
+## Integration tests
+
+1. Run `fastapi dev` in one shell
+2. Run `./run_integration_tests.sh` in another
+
+```bash
+❯ ./run_integration_tests.sh
+INFO:root:Testing interface with: Loopback432
+INFO:root:Get all interfaces
+DEBUG:urllib3.connectionpool:Starting new HTTP connection (1): 127.0.0.1:8000
+DEBUG:urllib3.connectionpool:http://127.0.0.1:8000 "GET /interfaces?host=sandbox-iosxr-1.cisco.com&device_type=iosxr HTTP/1.1" 200 4865
+INFO:root:Confirm interface Loopback432 missing
+DEBUG:urllib3.connectionpool:Starting new HTTP connection (1): 127.0.0.1:8000
+DEBUG:urllib3.connectionpool:http://127.0.0.1:8000 "GET /interface?host=sandbox-iosxr-1.cisco.com&device_type=iosxr&interface_name=Loopback432 HTTP/1.1" 404 63
+INFO:root:Create interface Loopback432
+DEBUG:urllib3.connectionpool:Starting new HTTP connection (1): 127.0.0.1:8000
+DEBUG:urllib3.connectionpool:http://127.0.0.1:8000 "POST /interface?host=sandbox-iosxr-1.cisco.com&device_type=iosxr HTTP/1.1" 200 45
+INFO:root:Confirm interface Loopback432 created
+DEBUG:urllib3.connectionpool:Starting new HTTP connection (1): 127.0.0.1:8000
+DEBUG:urllib3.connectionpool:http://127.0.0.1:8000 "GET /interface?host=sandbox-iosxr-1.cisco.com&device_type=iosxr&interface_name=Loopback432 HTTP/1.1" 200 455
+INFO:root:Delete interface Loopback432
+DEBUG:urllib3.connectionpool:Starting new HTTP connection (1): 127.0.0.1:8000
+DEBUG:urllib3.connectionpool:http://127.0.0.1:8000 "DELETE /interface?host=sandbox-iosxr-1.cisco.com&device_type=iosxr&interface_name=Loopback432 HTTP/1.1" 200 45
+INFO:root:Confirm interface Loopback432 deleted
+DEBUG:urllib3.connectionpool:Starting new HTTP connection (1): 127.0.0.1:8000
+DEBUG:urllib3.connectionpool:http://127.0.0.1:8000 "GET /interface?host=sandbox-iosxr-1.cisco.com&device_type=iosxr&interface_name=Loopback432 HTTP/1.1" 404 63
+```
+
 ## NGINX
 
 This command will start fastapi app and nginx in front of it on port 80: `docker compose up --build`
@@ -33,5 +65,5 @@ This will disable access directly to fastapi but you can get to it via nginx e.g
 ## TODO
 
 1. integration tests
-2. add dry run
-3. finish nexus add/remove
+2. finish nexus add/remove
+3. add fingerprinting to work out device_type automatically
---
+++ integration_tests.py
@@ -0,0 +1,97 @@
+"""
+Integration tests for this fast api app
+
+This file is designed to be run after the server is started in another shell
+"""
+
+import logging
+
+import requests
+
+logging.basicConfig(level="DEBUG")
+
+HOST = "sandbox-iosxr-1.cisco.com"
+DEVICE_TYPE = "iosxr"
+
+
+def test_interfaces():
+    """Test we can create and delete an interface"""
+    interface_name = "Loopback432"
+    logging.info("Testing interface with: %s", interface_name)
+
+    logging.info("Get all interfaces")
+    response = requests.get(
+        "http://127.0.0.1:8000/interfaces",
+        params={"host": HOST, "device_type": DEVICE_TYPE},
+        timeout=30,
+    )
+    assert response.status_code == 200
+
+    logging.info("Confirm interface %s missing", interface_name)
+    response = requests.get(
+        "http://127.0.0.1:8000/interface",
+        params={
+            "host": HOST,
+            "device_type": DEVICE_TYPE,
+            "interface_name": interface_name,
+        },
+        timeout=30,
+    )
+    assert response.status_code == 404
+
+    logging.info("Create interface %s", interface_name)
+    response = requests.post(
+        "http://127.0.0.1:8000/interface",
+        params={
+            "host": HOST,
+            "device_type": DEVICE_TYPE,
+        },
+        json={
+            "interface_name": interface_name,
+            "address": "10.1.1.2",
+            "netmask": "255.255.255.255",
+        },
+        timeout=30,
+    )
+    assert response.status_code == 200
+
+    logging.info("Confirm interface %s created", interface_name)
+    response = requests.get(
+        "http://127.0.0.1:8000/interface",
+        params={
+            "host": HOST,
+            "device_type": DEVICE_TYPE,
+            "interface_name": interface_name,
+        },
+        timeout=30,
+    )
+    assert response.status_code == 200
+
+    logging.info("Delete interface %s", interface_name)
+    response = requests.delete(
+        "http://127.0.0.1:8000/interface",
+        params={
+            "host": HOST,
+            "device_type": DEVICE_TYPE,
+            "interface_name": interface_name,
+        },
+        timeout=30,
+    )
+    assert response.status_code == 200
+
+    logging.info("Confirm interface %s deleted", interface_name)
+    response = requests.get(
+        "http://127.0.0.1:8000/interface",
+        params={
+            "host": HOST,
+            "device_type": DEVICE_TYPE,
+            "interface_name": interface_name,
+        },
+        timeout=30,
+    )
+    assert response.status_code == 404
+
+
+if __name__ == "__main__":
+    test_interfaces()
+    print("All assertions passed")
